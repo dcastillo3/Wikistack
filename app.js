54565146f1d199b9fe714ebd54f5c8680367c9e7
Daniel Castillo <dcastillo3@gmail.com>
2018-04-24 17:41:38 -0400

    setup sequelize

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
 const bodyParser = require('body-parser');
+const pg = require('pg');
+const Sequalize = require('sequelize');
+const db = new Sequalize('postgres://localhost:5432/wikistack');
 const app = express();
+
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+nunjucks.configure('views', {noCache: true});
+app.use(express.static('public'));
 // const routes = require('../routes');
 
 app.get('/', function(req, res, next) {
-    console.log('hello');
+    res.render('index', {
+        title: 'Wikistacks',
+    });
 });
 
 app.listen('3000');
